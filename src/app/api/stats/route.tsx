d0876f4b603e2301d657d136efcea859059b3256
horsefacts <webmaster@terminally.online>
2024-02-20 00:04:33 -0500

    re-cache leaderboard

--- src/app/api/stats/route.tsx
+++ src/app/api/stats/route.tsx
@@ -1,18 +1,21 @@
 import redis from "@/lib/redis";
 import { NextResponse } from "next/server";
 
+export const revalidate = 1800;
+
 export async function GET() {
   const flag = await redis.hgetall("flag");
   const { holderId, yoinkedAt } = flag;
-  const yoinks = (await redis.get("yoinks")) ?? "0";
+  const yoinks = await redis.get("yoinks") ?? "0";
   const userYoinksArray = await redis.zrevrange(
     "userYoinks",
     0,
     -1,
-    "WITHSCORES",
+    "WITHSCORES"
   );
   const userTimesArray = await redis.zrange("userTimes", 0, -1, "WITHSCORES");
   const users = await redis.hgetall("users");
+
 
   const arrayToObject = (arr: string[]) => {
     let obj: Record<string, number> = {};
@@ -25,22 +28,13 @@ export async function GET() {
   const userYoinks = arrayToObject(userYoinksArray);
   const userTimes = arrayToObject(userTimesArray);
 
-  if (holderId && yoinkedAt) {
-    const currentTime = Date.now();
-    const elapsedTime = Math.floor(
-      (currentTime - parseInt(yoinkedAt, 10)) / 1000,
-    );
-    userTimes[holderId] = (userTimes[holderId] || 0) + elapsedTime;
-    const [platform] = holderId.split(":");
-    userTimes[`platform:${platform}`] =
-      (userTimes[`platform:${platform}`] || 0) + elapsedTime;
-  }
+    if (holderId && yoinkedAt) {
+        const currentTime = Date.now();
+        const elapsedTime = Math.floor((currentTime - parseInt(yoinkedAt, 10)) / 1000);
+        userTimes[holderId] = (userTimes[holderId] || 0) + elapsedTime;
+        const [platform] = holderId.split(":");
+        userTimes[`platform:${platform}`] = (userTimes[`platform:${platform}`] || 0) + elapsedTime;
+    }
 
-  return NextResponse.json({
-    flag: { ...flag, yoinkedAt: parseInt(yoinkedAt) },
-    yoinks: parseInt(yoinks),
-    userYoinks,
-    userTimes,
-    users,
-  });
+  return NextResponse.json({ flag: {...flag, yoinkedAt: parseInt(yoinkedAt) }, yoinks: parseInt(yoinks), userYoinks, userTimes, users });
 }
