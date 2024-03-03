b62c14034754d2e3d814ea257ef975d8a2f99887
horsefacts <webmaster@terminally.online>
2024-03-03 02:37:18 -0700

    yoink cooldown is 1 minute

--- src/app/components/Leaderboard.tsx
+++ src/app/components/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import { Stats } from "./Yoink";
 import { formatCompact, formatHuman } from "@/lib/formatDuration";
 
