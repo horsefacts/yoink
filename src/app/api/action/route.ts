2ae82e94895a74cdc3686c2c6b1f4a2f379b62dd
horsefacts <webmaster@terminally.online>
2024-04-26 14:43:35 -0400

    add cast action

---
+++ src/app/api/action/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+const HOST = process.env["HOST"] || "https://yoink.terminally.online";
+
+export async function GET() {
+  return NextResponse.json({
+    name: "Yoink On Demand",
+    icon: "bookmark",
+    description: "Yoink anywhere, at any time.",
+    action: {
+        type: 'post',
+        postUrl: `${HOST}/api/action`
+    }
+  });
+}
+
+export async function POST() {
+  return NextResponse.json({ type: "frame", url: `${HOST}/api/start`});
+}
