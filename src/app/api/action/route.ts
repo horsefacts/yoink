6929b777560357119c81069a9229b67251be9bdc
horsefacts <webmaster@terminally.online>
2024-05-01 15:20:36 -0700

    update action name

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -4,7 +4,7 @@ const HOST = process.env["HOST"] || "https://yoink.terminally.online";
 
 export async function GET() {
   return NextResponse.json({
-    name: "Yoink On Demand",
+    name: "Yoink! On Demand",
     icon: "bookmark",
     description: "Yoink anywhere, at any time.",
     action: {
