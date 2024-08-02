1e9eacae621ad695e9f345a189dfd6266cba1d20
horsefacts <webmaster@terminally.online>
2024-08-01 23:20:19 -0400

    remove api routes

--- src/app/api/flag/route.tsx
+++
@@ -1,9 +0,0 @@
-import redis from "@/lib/redis";
-import { NextResponse } from "next/server";
-
-export const revalidate = 600;
-
-export async function GET() {
-  const flag = await redis.hgetall("flag");
-  return NextResponse.json(flag);
-}
